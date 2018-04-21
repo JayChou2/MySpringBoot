5a48a5eecaa5c3fdc57750e928c92cfe923368a2
JayChou2 <854944061@qq.com>
2018-04-21 18:58:14 +0800

    我的Spring Boot应用

--- SpringBoot/src/main/java/com/example/demo/MyController/HelloController.java
+++ SpringBoot/src/main/java/com/example/demo/MyController/HelloController.java
@@ -1,8 +1,6 @@
 package com.example.demo.MyController;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +10,7 @@ public class HelloController {
     @Autowired
     private Champion champion;
 
+    //test
     //@RequestParam  url中 获取问号传值    /welcome?name=abc
     //@PathVariable  url中 获取在路径中的值  /welcome/abc    上下相等
     //@RequestParam(value = "name",required = false,defaultValue = "爸爸")   required参数是否必输 defaultValue默认值
